49da1cc23e999d1389befcb32ce914b4f6fef85c
Kalimous <elsword485667@gmail.com>
2024-12-01 17:09:30 +0900

    add Configuration and Bean

--- src/test/java/com/naver/shopping/order/OrderServiceTest.java
+++ src/test/java/com/naver/shopping/order/OrderServiceTest.java
@@ -7,11 +7,14 @@ import com.naver.shopping.member.MemberService;
 import com.naver.shopping.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class OrderServiceTest {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = ac.getBean("memberService", MemberService.class);
+    OrderService orderService = ac.getBean("orderService", OrderService.class);
 
     @Test
     void 주문하기_고정할인() {
